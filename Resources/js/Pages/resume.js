59c497385e3f1ff608c243e0eb674bcaa9396900
Nate Claussen <natevan.email@gmail.com>
2021-02-11 20:07:36 -0700

    finished first visuals for resume page

---
+++ Resources/js/Pages/resume.js
@@ -0,0 +1,4 @@
+var dropDowns = document.getElementsByClassName("resumeCard");
+dropDowns.foreach(element => {
+    //addEventListener to make it open when clickd, or close... use CSS class ".up" ".down" (site.css) to make the arrow move up and down, and maybe determine if it is open or not and then just toggle that from there... Probably keep each tab open when you click on it and don't auto close each thing
+});
