c9175b9b87add09445ddec4709e3b7214777076d
Nate Claussen <natevan.email@gmail.com>
2021-02-18 19:27:50 -0700

    Finished JavaScript Final!

--- Resources/js/Pages/forFun.js
+++ Resources/js/Pages/forFun.js
@@ -40,6 +40,8 @@ const dashSymbol = "-";
 //Game Vars
 var lives = maxLives; //(head, body, leg, leg, arm, arm, eyes); might have to make it 6,
 var guessedLetters = [];
+var guessedRight = [];
+var guessRequired = 0;
 var RANDOM_WORD = "";
 const GameState = {
     CONTINUE: 0,
@@ -62,8 +64,11 @@ function restart() {
     //reset variables
     lives = 7;
     guessedLetters = [];
+    guessedRight = [];
     RANDOM_WORD = chooseRandomWord();
+    guessRequired = getRequiredRight();
     gameState = GameState.CONTINUE;
+    playAgainButton.style.display = "none";
     setStatusMessage("");
 }
 
@@ -91,8 +96,19 @@ function getPicture() {
 function chooseRandomWord() {
     //we don't need to worry about an index out of range, because the Math.floor() will make any number that is the max number - 1
     var word = wordBank[(Math.floor(Math.random() * wordBank.length))];
-    console.log("GAME: the word is: " + word);
     return word;
+}
+
+function getRequiredRight() {
+    var letters = [];
+    for (var i = 0; i < RANDOM_WORD.length; i++) {
+        if (!letters.includes(RANDOM_WORD[i])) {
+            if (RANDOM_WORD[i] != " ") {
+                letters.push(RANDOM_WORD[i]);
+            }
+        }
+    }
+    return letters.length;
 }
 
 function setDashes() {
@@ -131,48 +147,86 @@ function setStatusMessage(message) {
     document.getElementById("statusMessage").innerHTML = message;
 }
 
-function guessLetter(letter) {
-    guessedLetters.push(letter);
-
+function checkCorrect(letter) {
+    for (var i = 0; i < RANDOM_WORD.length; i++) {
+        if (letter === RANDOM_WORD[i]) return true;
+    }
+    return false;
 }
 
+function guessLetter(letter) {
+    if (guessedLetters.includes(letter)) {
+        setStatusMessage("You've already guessed that letter!");
+    } else {
+        //set status message if there is one or not...
+        guessedLetters.push(letter);
+        if (checkCorrect(letter)) {
+            guessedRight.push(letter);
+            setStatusMessage("You got a " + letter);
+        } else {
+            setStatusMessage("Sorry! No " + letter + "'s");
+            lives--;
+        }
+    }
+
+}
 
 function validateInput(input) {
     var regex = /^[A-Z]$/
     return regex.test(input);
 }
 
-function win() {
-    gameState = GameState.WIN;
-    render();
-    //set text to play again?
-    //When button is clicked, it will reset it (but the last game will stay there so they can see the word or be done)
+function checkWin() {
+    if (guessedRight.length === guessRequired) {
+        gameState = GameState.WIN;
+        return true;
+    }
+    return false;
 }
 
+function win() {
+    playAgainButton.style.display = "inline-block";
+}
+
+function lose() {
+    setStatusMessage("The word was '" + RANDOM_WORD + "'");
+    playAgainButton.style.display = "inline-block";
+}
+
+///the main brain behind everything
 function userGuess(event) {
     event.preventDefault();
+    if (gameState === GameState.LOSE) {
+        return false;
+    }
     var userInput = document.getElementById("userGuess").value.toUpperCase().trim();
-    if (validateInput(userInput)) { //validate input
-        //guess input
+    if (validateInput(userInput)) {
         guessLetter(userInput);
     } else {
-        //show error message if it is invalid input
         setStatusMessage("Please enter a valid letter! (and just one please)");
     }
 
-
-
-
-
-    //show results
+    inputForm.reset();
+    inputForm.focus();
+    if (lives <= 0) {
+        gameState = GameState.LOSE;
+        lose();
+    }
+    if (checkWin()) {
+        setStatusMessage("Congrats! You Win!");
+        win();
+    }
+    render();
 }
 
 var inputForm = document.getElementById("inputForm");
 inputForm.addEventListener("submit", userGuess, false);
 
+var playAgainButton = document.getElementById("playAgainButton");
+playAgainButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    startGame();
+}, false);
 
-
-
-
-
+///when the script loads, startGame();
 startGame();
